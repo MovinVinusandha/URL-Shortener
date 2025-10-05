e554f45702072ae51e57f0254a0bbd5898a1dc15
Movin Vinusandha <movinvinusandha@gmail.com>
2025-10-05 23:40:59 +0530

    feat(security): implement JWT validation using filters

--- src/main/java/com/url_shortener/url_shortener/services/JwtService.java
+++ src/main/java/com/url_shortener/url_shortener/services/JwtService.java
@@ -1,6 +1,8 @@
 package com.url_shortener.url_shortener.services;
 
 import com.url_shortener.url_shortener.entities.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,5 +26,29 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
+    }
+
+    public boolean validateToken(String token){
+        try {
+            var claims = getClaims(token);
+
+            return claims.getExpiration().after(new Date());
+        }
+        catch (JwtException e) {
+            return false;
+        }
+
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String getEmailFromToken(String token) {
+        return getClaims(token).getSubject();
     }
 }
