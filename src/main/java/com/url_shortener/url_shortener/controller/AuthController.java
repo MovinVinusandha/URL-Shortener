89cf190878d30b52c20a30bcc0b85e9a4daaf89b
Movin Vinusandha <movinvinusandha@gmail.com>
2025-10-06 09:13:04 +0530

    feat(security): implement refresh token issuance functionality

--- src/main/java/com/url_shortener/url_shortener/controller/AuthController.java
+++ src/main/java/com/url_shortener/url_shortener/controller/AuthController.java
@@ -1,11 +1,14 @@
 package com.url_shortener.url_shortener.controller;
 
+import com.url_shortener.url_shortener.config.JwtConfig;
 import com.url_shortener.url_shortener.dtos.JwtResponse;
 import com.url_shortener.url_shortener.dtos.LoginRequest;
 import com.url_shortener.url_shortener.dtos.UserDto;
 import com.url_shortener.url_shortener.mappers.UserMapper;
 import com.url_shortener.url_shortener.repositories.UserRepository;
 import com.url_shortener.url_shortener.services.JwtService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,11 +25,15 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final JwtConfig jwtConfig;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
     @PostMapping("/login")
-    public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest loginRequest){
+    public ResponseEntity<JwtResponse> login(
+            @Valid @RequestBody LoginRequest loginRequest,
+            HttpServletResponse response
+    ){
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         loginRequest.getEmail(),
@@ -35,9 +42,17 @@ public class AuthController {
         );
 
         var user = userRepository.findByEmail(loginRequest.getEmail()).orElseThrow();
-        var token = jwtService.generateToken(user);
+        var accessToken = jwtService.generateAccessToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
 
-        return ResponseEntity.ok(new JwtResponse(token));
+        var cookie = new Cookie("refreshToken", refreshToken);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/auth/refresh");
+        cookie.setMaxAge(jwtConfig.getRefreshTokenExpiration());
+        cookie.setSecure(true);
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok(new JwtResponse(accessToken));
     }
 
     @PostMapping("/validate")
